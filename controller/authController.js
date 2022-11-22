57eecb0657d5e44a4058ba50746073721d3cc66a
Tulasi Ram Reddy <tulasir287@gmail.com>
2022-11-22 11:41:19 +0530

    Added signin feature

--- controller/authController.js
+++ controller/authController.js
@@ -4,11 +4,19 @@
 
 const userModel = require('../model/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.signup = catchAsync(async (req, res) => {
-    console.log(1);
-    const newUser = await userModel.create({ email: req.body.email });
-    console.log(2);
+    if (req.body.role === 'admin') {
+        return new AppError('This Operation is not allowed to perform', 400);
+    }
+    const newUser = await userModel.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+        mobile: req.body.mobile
+    });
     return res.status(201).json({
         message: 'successfully created user',
         staus: 'success',
--- model/userModel.js
+++ model/userModel.js
@@ -1,14 +1,69 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-new */
+/* eslint-disable node/no-unsupported-features/es-syntax */
+
 const mongoose = require('mongoose');
 const validator = require('validator');
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
 
-const schema = mongoose.Schema({
-    email: {
-        type: 'string',
-        uniqure: true,
-        required: true,
-        lowercase: true,
-        validate: [validator.isEmail, 'Please enter valid email id']
-    }
+const schema = mongoose.Schema(
+    {
+        name: {
+            type: 'string',
+            required: [true, 'please provide your name']
+        },
+        role: {
+            type: 'string',
+            enum: ['student', 'institution', 'admin'],
+            default: 'student'
+        },
+        email: {
+            type: 'string',
+            unique: true,
+            required: [true, 'Please provide your email'],
+            lowercase: true,
+            validate: [validator.isEmail, 'Please enter valid email id']
+        },
+        password: {
+            type: 'string',
+            required: [true, 'Please provide your password'],
+            minlength: 8,
+            select: false
+        },
+        passwordConfirm: {
+            type: 'string',
+            required: [true, 'Please provide your confirm password'],
+            validate: {
+                validator: function(val) {
+                    return this.password === val;
+                },
+                message: 'password and passwordConfirm should be same'
+            }
+        },
+        mobile: {
+            type: 'string',
+            required: true,
+            unique: true
+            // validate: [validator.isMobilePhone(this.mobile)]
+        },
+        passwordChangedAt: Date,
+        passwordResetToken: String,
+        passwordRestTokenExpiresIn: Date,
+        active: {
+            type: 'boolean',
+            default: true,
+            select: false
+        }
+    },
+    { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+schema.pre('save', async function(next) {
+    if (!this.isModified('password')) next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const model = mongoose.model('users', schema);
